3b18226aa7cff032043d73d4f9a2fec8b9bf82f5
Gabriel Martinez <gibbi33@gmail.com>
2016-07-02 01:20:01 -0700

    Parsed data, adapted to chart.

--- main.js
+++ main.js
@@ -10,57 +10,70 @@ var randomColor = function() {
     return 'rgba(' + randomColorFactor() + ',' + randomColorFactor() + ',' + randomColorFactor() + ',.7)';
 };*/
 
-$.getJSON( "data/MIXEDBEV_06_2016.json", function( json ) {
 
-  dataarray = [];
-  dataarray.push(json[0].field10);
-  dataarray.push(json[1].field10);
 
-  /*var a = json[0].field10;
-  var b = json[1].field10;*/
 
-  /*anotherarray = [];
-  anotherarray.push(a);
-  anotherarray.push(b);*/
-  console.log(dataarray);
-  /*console.log('a: '+a);
-  console.log('b: '+b);
-  console.log('a+b: '+(a+b));*/
- });
-
-var ourlabels = function() {
-    //return ["January", "February", "March", "April", "May", "June", "July", "August"];
-    var thisarray = [];
-    for (i = 1; i < 51; i++) {
-      thisarray.push("Label: "+i);
-    }
-    return thisarray;
-};
-
-var ourvalues = function() {
-    //return [20, 30, 40, 50, 60, 70, 80, 90];
-    var thisarray = [];
-    for (i = 1; i < 51; i++) {
-      thisarray.push(i);
-    }
-    return thisarray;
-};
-
-var barChartData = {
-    labels: ourlabels(),
-    //labels: ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [{
-        label: '$',
-        backgroundColor: "rgba(220,220,220,0.5)",
-        //data: [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()]
-        //data: [20, 30, 40, 50, 60, 70, 80, 90]
-        data: ourvalues()
-    }]
-
-};
 
 window.onload = function() {
+
+  $.getJSON( "data/MIXEDBEV_06_2016.json", function( json ) {
+
+    countycheck = 'EL PASO';
+
+    var ourlabels = function() {
+        //return ["January", "February", "March", "April", "May", "June", "July", "August"];
+        var thisarray = [];
+        for (i = 0; i < json.length; i++) {
+          if ($.trim(json[i].field4) == countycheck){
+            thisarray.push(json[i].field2);
+          }
+        }
+        return thisarray;
+    };
+
+    var ourvalues = function() {
+        //return [20, 30, 40, 50, 60, 70, 80, 90];
+        var thisarray = [];
+        for (i = 0; i < json.length; i++) {
+          if ($.trim(json[i].field4) == countycheck){
+            thisarray.push(json[i].field10);
+          }
+        }
+        return thisarray;
+    };
+
+    //dataarray = [];
+    //dataarray.push(json[0].field10);
+    //dataarray.push(json[1].field10);
+
+    /*var a = json[0].field10;
+    var b = json[1].field10;*/
+
+    /*anotherarray = [];
+    anotherarray.push(a);
+    anotherarray.push(b);*/
+    //console.log(json);
+    /*console.log('a: '+a);
+    console.log('b: '+b);
+    console.log('a+b: '+(a+b));*/
+
+
+
+  var barChartData = {
+      labels: ourlabels(),
+      //labels: ["January", "February", "March", "April", "May", "June", "July"],
+      datasets: [{
+          label: '$',
+          backgroundColor: "rgba(220,220,220,0.5)",
+          //data: [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()]
+          //data: [20, 30, 40, 50, 60, 70, 80, 90]
+          data: ourvalues()
+      }]
+
+  };
+
     var ctx = document.getElementById("canvas").getContext("2d");
+
     window.myBar = new Chart(ctx, {
         type: 'bar',
         data: barChartData,
@@ -70,7 +83,7 @@ window.onload = function() {
             elements: {
                 rectangle: {
                     borderWidth: 2,
-                    borderColor: 'rgb(0, 255, 0)',
+                    borderColor: 'rgb(74, 150, 226)',
                     borderSkipped: 'bottom'
                 }
             },
@@ -86,60 +99,64 @@ window.onload = function() {
         }
     });
 
-};
 
-$('#randomizeData').click(function() {
-    var zero = Math.random() < 0.2 ? true : false;
-    $.each(barChartData.datasets, function(i, dataset) {
-        dataset.backgroundColor = randomColor();
-        dataset.data = dataset.data.map(function() {
-            return zero ? 0.0 : randomScalingFactor();
+
+    /*$('#randomizeData').click(function() {
+        var zero = Math.random() < 0.2 ? true : false;
+        $.each(barChartData.datasets, function(i, dataset) {
+            dataset.backgroundColor = randomColor();
+            dataset.data = dataset.data.map(function() {
+                return zero ? 0.0 : randomScalingFactor();
+            });
+
         });
-
+        window.myBar.update();
     });
-    window.myBar.update();
-});
 
-$('#addDataset').click(function() {
-    var newDataset = {
-        label: 'Dataset ' + barChartData.datasets.length,
-        backgroundColor: randomColor(),
-        data: []
-    };
+    $('#addDataset').click(function() {
+        var newDataset = {
+            label: 'Dataset ' + barChartData.datasets.length,
+            backgroundColor: randomColor(),
+            data: []
+        };
 
-    for (var index = 0; index < barChartData.labels.length; ++index) {
-        newDataset.data.push(randomScalingFactor());
-    }
-
-    barChartData.datasets.push(newDataset);
-    window.myBar.update();
-});
-
-$('#addData').click(function() {
-    if (barChartData.datasets.length > 0) {
-        var month = MONTHS[barChartData.labels.length % MONTHS.length];
-        barChartData.labels.push(month);
-
-        for (var index = 0; index < barChartData.datasets.length; ++index) {
-            //window.myBar.addData(randomScalingFactor(), index);
-            barChartData.datasets[index].data.push(randomScalingFactor());
+        for (var index = 0; index < barChartData.labels.length; ++index) {
+            newDataset.data.push(randomScalingFactor());
         }
 
+        barChartData.datasets.push(newDataset);
         window.myBar.update();
-    }
-});
-
-$('#removeDataset').click(function() {
-    barChartData.datasets.splice(0, 1);
-    window.myBar.update();
-});
-
-$('#removeData').click(function() {
-    barChartData.labels.splice(-1, 1); // remove the label first
-
-    barChartData.datasets.forEach(function(dataset, datasetIndex) {
-        dataset.data.pop();
     });
 
-    window.myBar.update();
+    $('#addData').click(function() {
+        if (barChartData.datasets.length > 0) {
+            var month = MONTHS[barChartData.labels.length % MONTHS.length];
+            barChartData.labels.push(month);
+
+            for (var index = 0; index < barChartData.datasets.length; ++index) {
+                //window.myBar.addData(randomScalingFactor(), index);
+                barChartData.datasets[index].data.push(randomScalingFactor());
+            }
+
+            window.myBar.update();
+        }
+    });
+
+    $('#removeDataset').click(function() {
+        barChartData.datasets.splice(0, 1);
+        window.myBar.update();
+    });
+
+    $('#removeData').click(function() {
+        barChartData.labels.splice(-1, 1); // remove the label first
+
+        barChartData.datasets.forEach(function(dataset, datasetIndex) {
+            dataset.data.pop();
+        });
+
+        window.myBar.update();
+    });*/
+
 });
+
+};
