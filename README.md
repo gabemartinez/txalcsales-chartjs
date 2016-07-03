ba5584a02d793b21d17604f569937df42e1cfada
Gabriel Martinez <gibbi33@gmail.com>
2016-07-03 00:32:30 -0700

    Added zip code options, and removed files.

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # txalcsales-chartjs
-Texas Mixed Beverage Gross Receipts Tax Receipts - A Chart.js Project
+Texas Mixed Beverage Gross Tax Receipts - A single-page Chart.js project.
+
+The visualizations represent the mixed beverage tax data reported by El Paso County taxpayers (bars/restaurants) under Chapter 183 of the Texas Tax Code.
+Using the form below, enter a Sales Greater Than threshold (not required), zip code (not required), and then click a month of data to update visualization.
+On initial load, the most recent month of data with no options selected will be show in the chart. Hover over a bar to view establishment name, and other details.
 
 [Data Source](http://www.texastransparency.org/Data_Center/Search_Datasets.php)
 
@@ -9,5 +13,5 @@ Texas Mixed Beverage Gross Receipts Tax Receipts - A Chart.js Project
 # Run Locally With SimpleHTTPServer
 python -m SimpleHTTPServer 8888
 
-# View Work in Progress
-[Click here.](http://www.txalcsales.martinezgabriel.com/)
+# Use WIP-App
+[Available here.](http://www.txalcsales.martinezgabriel.com/)
--- js/bundle.js
+++ js/bundle.js
@@ -3,76 +3,103 @@ $(document).ready(function() {
 
   $('#decemberData').click(function() {
       var totalcheck = $('#totalcheck').val();
+      var zipcheck = $('#zipcheck').val();
       myBar.destroy();
       myBar.update();
-      runmychart('data/MIXEDBEV_12_2015.json', 'EL PASO', totalcheck);
+      runmychart('data/MIXEDBEV_12_2015.json', 'EL PASO', totalcheck, zipcheck);
   });
 
   $('#januaryData').click(function() {
       var totalcheck = $('#totalcheck').val();
+      var zipcheck = $('#zipcheck').val();
       myBar.destroy();
       myBar.update();
-      runmychart('data/MIXEDBEV_01_2016.json', 'EL PASO', totalcheck);
+      runmychart('data/MIXEDBEV_01_2016.json', 'EL PASO', totalcheck, zipcheck);
   });
 
   $('#februaryData').click(function() {
       var totalcheck = $('#totalcheck').val();
+      var zipcheck = $('#zipcheck').val();
       myBar.destroy();
       myBar.update();
-      runmychart('data/MIXEDBEV_02_2016.json', 'EL PASO', totalcheck);
+      runmychart('data/MIXEDBEV_02_2016.json', 'EL PASO', totalcheck, zipcheck);
   });
 
   $('#marchData').click(function() {
       var totalcheck = $('#totalcheck').val();
+      var zipcheck = $('#zipcheck').val();
       myBar.destroy();
       myBar.update();
-      runmychart('data/MIXEDBEV_03_2016.json', 'EL PASO', totalcheck);
+      runmychart('data/MIXEDBEV_03_2016.json', 'EL PASO', totalcheck, zipcheck);
   });
 
   $('#aprilData').click(function() {
       var totalcheck = $('#totalcheck').val();
+      var zipcheck = $('#zipcheck').val();
       myBar.destroy();
       myBar.update();
-      runmychart('data/MIXEDBEV_04_2016.json', 'EL PASO', totalcheck);
+      runmychart('data/MIXEDBEV_04_2016.json', 'EL PASO', totalcheck, zipcheck);
   });
 
   $('#mayData').click(function() {
       var totalcheck = $('#totalcheck').val();
+      var zipcheck = $('#zipcheck').val();
       myBar.destroy();
       myBar.update();
-      runmychart('data/MIXEDBEV_05_2016.json', 'EL PASO', totalcheck);
+      runmychart('data/MIXEDBEV_05_2016.json', 'EL PASO', totalcheck, zipcheck);
   });
 
   $('#juneData').click(function() {
       var totalcheck = $('#totalcheck').val();
+      var zipcheck = $('#zipcheck').val();
       myBar.destroy();
       myBar.update();
-      runmychart('data/MIXEDBEV_06_2016.json', 'EL PASO', totalcheck);
+      runmychart('data/MIXEDBEV_06_2016.json', 'EL PASO', totalcheck, zipcheck);
   });
 
-  var runmychart = function(datafile, countycheck, totalcheck){
+  var runmychart = function(datafile, countycheck, totalcheck, zipcheck){
 
     $.getJSON(datafile, function(json) {
 
       var ourlabels = function() {
           //return ["January", "February", "March", "April", "May", "June", "July", "August"];
           var thisarray = [];
-          for (i = 0; i < json.length; i++) {
-            if (json[i].field10 > totalcheck && $.trim(json[i].field4) == countycheck){
-              thisarray.push(json[i].field2 + json[i].field3);
+
+          if (zipcheck === ''){
+            for (i = 0; i < json.length; i++) {
+              if (json[i].field10 > totalcheck && $.trim(json[i].field4) == countycheck){
+                thisarray.push(json[i].field2 + json[i].field3);
+              }
+            }
+          } else {
+            for (i = 0; i < json.length; i++) {
+              if (json[i].field10 > totalcheck && $.trim(json[i].field4) == countycheck && json[i].field6 == zipcheck){
+                thisarray.push(json[i].field2 + json[i].field3);
+              }
             }
           }
+
           return thisarray;
       };
 
       var ourvalues = function() {
           //return [20, 30, 40, 50, 60, 70, 80, 90];
           var thisarray = [];
-          for (i = 0; i < json.length; i++) {
-            if (json[i].field10 > totalcheck && $.trim(json[i].field4) == countycheck){
-              thisarray.push(json[i].field10);
+
+          if (zipcheck === ''){
+            for (i = 0; i < json.length; i++) {
+              if (json[i].field10 > totalcheck && $.trim(json[i].field4) == countycheck){
+                thisarray.push(json[i].field10);
+              }
+            }
+          } else {
+            for (i = 0; i < json.length; i++) {
+              if (json[i].field10 > totalcheck && $.trim(json[i].field4) == countycheck && json[i].field6 == zipcheck){
+                thisarray.push(json[i].field10);
+              }
             }
           }
+
           return thisarray;
       };
 
@@ -108,8 +135,28 @@ $(document).ready(function() {
                   position: 'top',
               },
               title: {
-                  display: true,
+                  display: false,
                   text: 'Data: ' + datafile + ' || ' + 'County: ' + countycheck + ' || ' + 'Total greater than: $' +  totalcheck
+              },
+              scales:
+              {
+                  xAxes: [{
+                      display: false
+                  }],
+                  yAxes: [{
+                      ticks: {
+                          max: 25000,
+                          min: 0,
+                          stepSize: 5000,
+                          callback: function(value, index, values) {
+                            if(parseInt(value) > 1000){
+                              return '$' + value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                            } else {
+                              return '$' + value;
+                            }
+                          }
+                      }
+                  }]
               }
           }
       });
@@ -118,7 +165,7 @@ $(document).ready(function() {
 
   };
 
-  runmychart('data/MIXEDBEV_06_2016.json', 'EL PASO', 1000);
+  runmychart('data/MIXEDBEV_06_2016.json', 'EL PASO', 1000, '');
 
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
