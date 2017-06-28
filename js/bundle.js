1b902bb49879e200a0985ffce50044871fd79a7d
Gabriel Martinez <gibbi33@gmail.com>
2017-06-27 21:53:44 -0700

    Cleaned data.

--- js/bundle.js
+++ js/bundle.js
@@ -140,7 +140,7 @@ $(document).ready(function() {
 
   };
 
-  runmychart('data/MIXEDBEV_06_2017.json', 71, 0, 'all', 'April - 2017');
+  runmychart('data/MIXEDBEV_06_2017.json', 71, 0, 'all', 'June - 2017');
 
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
